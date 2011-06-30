acfabf2a4df3571591fcb8822b3e2413f7d8f061
Sheldon Chen <zipinghappy@hotmail.com>
2011-06-30 21:03:03 +0800

    first commit

---
+++ structured/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * User: sheldon
+ * Date: 11-6-30
+ * Time: 下午8:40
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
